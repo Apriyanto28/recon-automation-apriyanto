73c07597f6246fd48f658fd6c4c9a8c8b7b318ac
Apriyanto28 <apriyanto.halim@mikroskil.ac.id>
2025-11-14 23:16:36 +0700

    Menambahkan file yang digunakan untuk menjalankan recon otomatis

---
+++ scripts/recon-auto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# cara penggunaan perintah: 
+# - Terlebih dahulu jadikan file script-auto.sh menjadi script yang bisa dijalankan
+# dengan perinth: chmod +x script-auto.sh
+# - Jalan script ini dengan perintah ./script-auto.sh [daftar domain yang mau di-recon
