1700b62b237172079babb4b9c5d0358030202d6a
Apriyanto28 <apriyanto.halim@mikroskil.ac.id>
2025-11-14 23:28:59 +0700

    Menambahkan perintah peencegahan ke dalaam file auto-recon.sh

--- scripts/recon-auto.sh
+++ scripts/recon-auto.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+
 # cara penggunaan perintah: 
 # - Terlebih dahulu jadikan file script-auto.sh menjadi script yang bisa dijalankan
 # dengan perinth: chmod +x script-auto.sh
 # - Jalan script ini dengan perintah ./script-auto.sh [daftar domain yang mau di-recon
+
+
+# Perintah ini digunakan untuk mencegah terjadinya kesalahan pada perintah pada saat 
+# dijalankan
+
+# Perintah ini digunakan untuk menghentikan perintah apabila ada yang tidak berjalan
+set -o errexit
+
+# Perintah ini digunakan menghentikan perintah apabila variabel tersebut belum dikenalkan
+# sebelumnya
+set -o nounset
+
+# Perintah ini digunakan untuk menghentikan proses apabila salah satu perintah pada 
+# pipeline tidak berhasil dijalankan
+set -o pipefail
